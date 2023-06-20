2dc56807e0ed601f22464992df8f69f4160bf0d9
suzukidavid <superdevk@gmail.com>
2023-06-20 16:33:26 +0600

    add income serializers

---
+++ backend/income/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Income
+
+
+class IncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Income
+        fields = '__all__'
